8735a25df2de244fa7981e1d8b3eb1dd9c2ad564
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2018-09-23 10:51:22 -0600

    Holas

--- ajax/procesar_login.php
+++ ajax/procesar_login.php
@@ -7,13 +7,17 @@ $link = new Conexion();
 $respuesta = Empleado::verificarEmpleado($link, $_POST["txt-username-login"], $_POST["txt-clave-login"]);
 
 if ($respuesta["existe"])
-{
-	$_SESSION["dni"] = $respuesta["dni"];
-	$_SESSION["afiliacion"] = $respuesta["afiliacion"];
-	$_SESSION["nombre"] = $respuesta["nombre"];
-	$_SESSION["username"] = $respuesta["username"];
-	$_SESSION["isAdmin"] = $respuesta["isAdmin"];
-	echo json_encode($respuesta);
+{	
+	if ($respuesta["tipo"]=="1"||$respuesta["isAdmin"]==true) {
+		$_SESSION["dni"] = $respuesta["dni"];
+		$_SESSION["afiliacion"] = $respuesta["afiliacion"];
+		$_SESSION["nombre"] = $respuesta["nombre"];
+		$_SESSION["username"] = $respuesta["username"];
+		$_SESSION["isAdmin"] = $respuesta["isAdmin"];
+		echo json_encode($respuesta);
+	} else {
+		echo "Empleado no existe!";
+	}
 }
 else
 {
